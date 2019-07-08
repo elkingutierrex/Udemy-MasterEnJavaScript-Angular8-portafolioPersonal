abe5db64ea3f023f7f8507a9fc68c98e312c059b
elkingutierrex <elkin.gutierrex@gmail.com>
2019-07-08 11:33:35 -0500

    242 - Página sobre Mí

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -6,8 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  public title: string;
+  public subtitle: string;
+  public web: string;
 
-  constructor() { }
+  constructor() {
+    this.title = "elkingutierrex";
+    this.subtitle="Desarrollador de software fullstack";
+    this.web = "elkin.gutierrex@gmail.com"
+
+   }
 
   ngOnInit() {
   }
