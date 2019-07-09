fa4834a2999777c25822eedb78a8e933ce96e55e
elkingutierrex <elkin.gutierrex@gmail.com>
2019-07-09 15:44:22 -0500

    243 - Modelos y servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {routing, appRoutingProviders} from './app.routing';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
