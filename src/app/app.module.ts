08aff0cc697cd212cee0af6bb9a10e81ada5fde7
elkingutierrex <elkin.gutierrex@gmail.com>
2019-07-09 15:47:09 -0500

    243 - Modelos y servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,9 @@ import { ErrorComponent } from './components/error/error.component';
   imports: [
     BrowserModule,
     //AppRoutingModule,
-    routing
+    routing,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     appRoutingProviders
