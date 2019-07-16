5a179e92bac25edd6321b3d50e4b701472a647db
elkingutierrex <elkin.gutierrex@gmail.com>
2019-07-16 16:12:31 -0500

    247 - Guardar nuevos proyectos en el portafolio

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -16,4 +16,10 @@ this.url = Global.url
   testService(){
     return 'Probando el modelo de angular'
   }
+  saveProject(project : Project): Observable <any>{
+    let params = JSON.stringify(project);
+    let headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+    return this._http.post(this.url + '/save-project', params, {headers:headers})
+  }
 }
