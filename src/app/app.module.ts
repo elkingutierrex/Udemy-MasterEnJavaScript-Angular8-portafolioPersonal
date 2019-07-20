627896b2a0e79cac4af9536badf9ea9564bacb74
elkingutierrex <elkin.gutierrex@gmail.com>
2019-07-20 08:13:20 -0500

    263 - Directivas personalizadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ErrorComponent } from './components/error/error.component';
 import { DetailComponent } from './components/detail/detail.component';
 import { EditComponent } from './components/edit/edit.component';
 import { SliderComponent } from './components/slider/slider.component';
+import { ResaltadoDirective } from './resaltado.directive';
 
 //import * as $ from "jquery"
 
@@ -27,7 +28,8 @@ import { SliderComponent } from './components/slider/slider.component';
     ErrorComponent,
     DetailComponent,
     EditComponent,
-    SliderComponent
+    SliderComponent,
+    ResaltadoDirective
   ],
   imports: [
     BrowserModule,
